ea1f92a19cc705e58caead2562dcc1d855f4bb62
Feerdus95 <ferdus95.fv@gmail.com>
2023-02-07 17:33:28 -0300

    Commit inicial

---
+++ src/com/example/Main.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Numéricos
+        byte bytecito = 1;
+        System.out.println("El byte es: " +bytecito);
+        short shortcito = 2;
+        System.out.println("El short es: " +shortcito);
+        int integercito = 4;
+        System.out.println("El integer es: " +integercito);
+        long longcito = 8;
+        System.out.println("El long es: " +longcito);
+        float floatcito = 9.99f;
+        System.out.println("El float es: " +floatcito);
+        double doublecito = 99.99d;
+        System.out.println("El double es: " +doublecito);
+
+        //Booleanos
+        boolean verdadero = true;
+        System.out.println(verdadero);
+        boolean falso = false;
+        System.out.println(falso);
+
+        //Texto
+        char charcito = 'a';
+        System.out.println("El char es: " +charcito);
+        String stringcito = "Stringcito";
+        System.out.println("El string es: " +stringcito);
+    }
+}
